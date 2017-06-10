3a6dbe7c71a210872fc25c29d4c9293a99b97ccd
Simone Shibata <simone_shibata@hotmail.com>
2017-06-10 15:41:56 -0300

    aula 02 - 10-06-2017 - proxy pattern

--- src/test/java/pos_padrao/pos_padrao/CollectionProxyCreatorTest.java
+++ src/test/java/pos_padrao/pos_padrao/CollectionProxyCreatorTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class CollectionProxyCreatorTest {
 
 	@Test
-	public void test() {
+	public void testAddItemEmProxy() {
 		
 		Produto produto = new Produto();
 		
@@ -19,14 +19,30 @@ public class CollectionProxyCreatorTest {
 		
 		List<Produto> lista = repository.findAll();
 		List proxy = CollectionProxyCreator.createUnmodifiableList(lista);
+		Produto produto2 = new Produto();
 		
 		try {
-			Produto produto2 = new Produto();
 			proxy.add(produto2);
 			fail("Não deveria deixar adicionar");
 		} catch (Exception e) {
 		}
 		
+		Assert.assertTrue(proxy.contains(produto));
+		Assert.assertFalse(proxy.contains(produto2));
+		
+	}
+	
+	@Test
+	public void testRemoveItemEmProxy() {
+		
+		Produto produto = new Produto();
+		
+		ProdutoRepositoryTransient repository = new ProdutoRepositoryTransient();
+		repository.save(produto);
+		
+		List<Produto> lista = repository.findAll();
+		List proxy = CollectionProxyCreator.createUnmodifiableList(lista);
+
 		try {
 			proxy.remove(produto);
 			fail("Não deveria deixar remover");
